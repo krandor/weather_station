e61f4b4ce64890e89f120699ad915c6ccf27557e
krandor <krandor.delmarniol@gmail.com>
2016-06-09 10:29:38 -0400

    wrap display code in wrapper class

--- tempMonitor.py
+++ tempMonitor.py
@@ -9,7 +9,6 @@ import ImageFont
 from smbus import SMBus
 from sys import exit
 
-
 # Special characters
 deg = u'\N{DEGREE SIGN}'
 
@@ -74,10 +73,53 @@ MPL3115A2_CTRL_REG5 = 0x2A
 
 MPL3115A2_REGISTER_STARTCONVERSION = 0x12
 
-_bus = SMBus(0)
 
-# 128x32 display with hardware I2C:
-_display = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=0)
+class DisplayWrapper:
+    _display = None
+    _draw = None
+    _image = None
+    _font = None
+    _height = 0
+    _width = 0
+
+    def __init__(self):
+        # 128x32 display with hardware I2C:
+        self._display = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=0)
+        # Initialize library.
+        self._display.begin()
+        # Clear display.
+        self._display.clear()
+        self._display.display()
+        # Create blank image for drawing.
+        # Make sure to create image with mode '1' for 1-bit color.
+        self._width = self._display.width
+        self._height = self._display.height
+        self._image = Image.new('1', (self._width, self._height))
+        # Get drawing object to draw on image.
+        self._draw = ImageDraw.Draw(self._image)
+        # Load default font.
+        self._font = ImageFont.load_default()
+
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def width(self):
+        return self._width
+
+    def clearDisplay(self):
+        self._draw.rectangle((0, 0, self._width, self._height), outline=0, fill=0)
+
+    def drawText(self, texttowrite, x, y):
+        self._draw.text((x, y), texttowrite, font=self._font, fill=255)
+
+    def displayImage(self):
+        self._display.image(self._image)
+        self._display.display()
+
+
+_bus = SMBus(0)
 
 whoami = _bus.read_byte_data(MPL3115A2_ADDRESS, MPL3115A2_WHOAMI)
 
@@ -182,76 +224,58 @@ def temperature():
     poll()
 
     t_data = _bus.read_i2c_block_data(MPL3115A2_ADDRESS, 0x04, 2)
-    status = _bus.read_byte_data(MPL3115A2_ADDRESS, 0x00)
+    #status = _bus.read_byte_data(MPL3115A2_ADDRESS, 0x00)
 
     # print t_data
 
     return t_data[0] + (t_data[1] >> 4) / 16.0
 
 
+dw = DisplayWrapper()
+
 try:
-    # Initialize library.
-    _display.begin()
-
-    # Clear display.
-    _display.clear()
-    _display.display()
-
-    # Create blank image for drawing.
-    # Make sure to create image with mode '1' for 1-bit color.
-    width = _display.width
-    height = _display.height
-    image = Image.new('1', (width, height))
-
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
-
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    dw.clearDisplay()
+    # draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     # Draw some shapes.
     # First define some constants to allow easy resizing of shapes.
     padding = 2
     shape_width = 20
     top = padding
-    bottom = height - padding
+    bottom = dw.height() - padding
     # Move left to right keeping track of the current x position for drawing shapes.
     x = 5
 
-    # Load default font.
-    font = ImageFont.load_default()
+    dw.clearDisplay()
+    dw.drawText('Calibrating...', x, top)
+    dw.displayImage()
 
-    draw.rectangle((0, 0, width, height), outline=255, fill=0)
-    draw.text((x, top), 'Calibrating...', font=font, fill=255)
-    _display.image(image)
-    _display.display()
-
-    avgs = calibrate()
+    calibrate()
 
     i = 0
 
-    while (1):
-        # clear display
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    while 1:
         # get readings
         tempC = temperature()  # temp in Celcius
         tempF = (tempC * 1.8) + 32
         press = (pressure() / 1000)  # get pressure and convert to kPa
+        # clear display
+        dw.clearDisplay()
         # draw readings to image
-        draw.text((x, top), 'TempC: ' + "{0:.2f}".format(tempC) + deg + ' C', font=font, fill=255)
-        draw.text((x, top + 10), 'TempF: ' + "{0:.2f}".format(tempF) + deg + ' F', font=font, fill=255)
-        draw.text((x, top + 20), 'Pressure: ' + "{0:.2f}".format(press) + ' kPa', font=font, fill=255)
+        dw.drawText('TempC: ' + "{0:.2f}".format(tempC) + deg + ' C', x, top)
+        dw.drawText('TempF: ' + "{0:.2f}".format(tempF) + deg + ' F', x, top + 10)
+        dw.drawText('Pressure: ' + "{0:.2f}".format(press) + ' kPa', x, top + 20)
         # Display image.
-        _display.image(image)
-        _display.display()
+        dw.displayImage()
 
 except OSError as err:
     print("OS Error: {0}".format(err))
-    _display.clear()
-    _display.display()
+    dw.clearDisplay()
+    dw.displayImage()
     sys.exit(1);
 except KeyboardInterrupt:
     print("Keyboard Interrupt detected")
-    _display.clear()
-    _display.display()
+    dw.clearDisplay()
+    dw.displayImage()
     sys.exit(0);
